875013e22bb91204fa8a5af4f32b28a050a44697
谭龙 <755956503@qq.com>
2018-11-03 16:54:29 +0800

    mobx

---
+++ mobx/src/pages/page1/index.js
@@ -0,0 +1,18 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+const propTypes = {
+};
+const defaultProps = {
+};
+class Page1 extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    
+  }
+
+}
+Page1.propTypes = propTypes;
+Page1.defaultProps = defaultProps;
+export default Page1;
